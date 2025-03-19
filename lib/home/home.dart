4339a26f2b7b89661a4459c79d3b6de739f8be57
aliakrem <barka.aliakrem@gmail.com>
2025-03-19 17:13:00 +0100

    style: fix naming

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:oopquiz/blocs/win_animation_bloc/win_animation_bloc.dart';
+import 'package:oopquiz/Blocs/win_animation_bloc/win_animation_bloc.dart';
 import 'package:oopquiz/shared/shared.dart';
 import 'package:flutter/material.dart';
 
--- lib/shared/quiz_congrats_page.dart
+++ lib/shared/quiz_congrats_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:oopquiz/blocs/win_animation_bloc/win_animation_bloc.dart';
+import 'package:oopquiz/Blocs/win_animation_bloc/win_animation_bloc.dart';
 import 'package:oopquiz/models/topic_model.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
